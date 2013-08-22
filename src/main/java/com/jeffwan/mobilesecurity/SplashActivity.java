2f7fdcc14ebbba4073d4e4f738a5fffffcd82b1a
Jiaxin Shan <seedjeffwan@gmail.com>
2013-08-21 22:32:20 -0400

    1.add getVersion function 2.add Xml parse function to get server updateinfo.xml

--- src/main/java/com/jeffwan/mobilesecurity/SplashActivity.java
+++ src/main/java/com/jeffwan/mobilesecurity/SplashActivity.java
@@ -4,11 +4,23 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
+import com.jeffwan.mobilesecurity.domain.UpdateInfo;
+import com.jeffwan.mobilesecurity.engine.UpdateInfoParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class SplashActivity extends Activity {
+    private static final String TAG = "SplashActivity" ;
     private TextView tv_splash_version;
+    private UpdateInfo updateInfo;
 
     // package manger -> Android apk Manager
     private PackageManager pm;
@@ -24,7 +36,45 @@ public class SplashActivity extends Activity {
 
 
 
+
+
     }
+
+    private class checkVersion implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(getResources().getString(R.string.serverUrl));
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setReadTimeout(5000);
+
+                int code = conn.getResponseCode();
+                if(code ==200){
+                    InputStream is = conn.getInputStream();
+                    updateInfo = UpdateInfoParser.UpdateInfoParser(is);
+
+                    if(updateInfo!=null){
+                        Log.i(TAG,"parse successfully");
+                    }else {
+                        Log.i(TAG, "parse fail");
+                    }
+
+                }else{
+                    //TODO: server problem
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
 
 
     public String getAppVersion(){
---
+++ src/main/java/com/jeffwan/mobilesecurity/engine/UpdateInfoParser.java
@@ -0,0 +1,54 @@
+package com.jeffwan.mobilesecurity.engine;
+
+import android.util.Xml;
+
+import com.jeffwan.mobilesecurity.domain.UpdateInfo;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by jeffwan on 8/21/13.
+ */
+public class UpdateInfoParser {
+
+    public UpdateInfo getUpdateInfo(InputStream is) {
+        XmlPullParser parser = Xml.newPullParser();
+        UpdateInfo info = new UpdateInfo();
+        try {
+            parser.setInput(is,"UTF-8");
+            int type = parser.getEventType();
+            while(type != XmlPullParser.END_DOCUMENT){
+                switch (type){
+                    case XmlPullParser.START_TAG:
+                        if ("version".equals(parser.getName())){
+                            String version = parser.nextText();
+                            info.setVerison(version);
+                        }else if ("description".equals(parser.getName())){
+                            String description = parser.nextText();
+                            info.setDescription(description);
+                        }else if ("url".equals(parser.getName())){
+                            String url = parser.nextText();
+                            info.setUrl(url);
+                        }
+                        break;
+                }
+                type = parser.next();
+            }
+            is.close();
+            return info;
+
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+}
